699850d4339c85cafb62ea19c87c1cb7d3b14645
Eco <osw2156@gmail.com>
2021-10-18 19:36:17 -0600

    Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
-  template: '<h1>My Ok</h1>'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
